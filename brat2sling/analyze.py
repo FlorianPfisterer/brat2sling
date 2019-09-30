bb1b5fcff971a25915f32c478de5bf393dca994e
Florian Pfisterer <florian.pfisterer.2@gmail.com>
2019-09-30 15:12:58 -0400

    Add doc analyzer and analyze script [WIP]

---
+++ brat2sling/analyze.py
@@ -0,0 +1,24 @@
+from bratreader.repomodel import RepoModel
+from brat2sling.doc_converter import DocConverter
+from brat2sling.doc_reader import DocReader
+from brat2sling.doc_analyzer import DocAnalyzer
+import argparse
+
+def analyze(brat_dir_path: str):
+    # load the brat repository
+    repo = RepoModel(brat_dir_path)
+
+    for document_name in repo.documents:
+        document = repo.documents[document_name]
+        reader = DocReader(document)
+        DocAnalyzer().analyze(reader)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--brat_dir_path', dest='brat_dir_path', required=True,
+                        help='The path to the directory holding the brat files.')
+    args = parser.parse_args()
+    analyze(args.brat_dir_path)
+
+
---
+++ brat2sling/doc_analyzer.py
@@ -0,0 +1,14 @@
+from typing import Union, List, Dict
+
+from brat2sling.brat_frames import BRAT_TO_SLING_FRAME, BRAT_TO_SLING_RELATION
+from brat2sling.doc_reader import DocReader
+from brat2sling.slot import Slot
+
+
+class DocAnalyzer:
+    def analze(self, reader: DocReader):
+        # 1. add all tokens
+        tokens = reader.get_tokens()
+
+        # 2. add all mentions
+        mentions = reader.get_all_mentions()
